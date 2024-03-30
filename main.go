fa1082da56c8fe489dbfba16dd4f63c45212c5e8
iamtonmoy0 <tonmoy2code@gmail.com>
2024-03-30 15:20:19 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+	r := Repository{
+		DB:db
+	}
+
+	app := fiber.New()
+	r.SetupRoutes(app)
+
+	app.Listen(":3000")
+}
